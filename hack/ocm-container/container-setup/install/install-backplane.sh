257a4b498831c76249bb0f1b786c5d05139b8d15
Supreeth Basabattini <sbasabat@redhat.com>
2023-01-27 15:49:03 +0530

    Makefile changes + ocm-container addition

---
+++ hack/ocm-container/container-setup/install/install-backplane.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -e
+
+if [ "$I_AM_IN_CONTAINER" != "I-am-in-container" ]; then
+  echo "must be run in container";
+  exit 1;
+fi
+
+echo "in container";
+
+mkdir /usr/local/backplane -p
+pushd /usr/local/backplane
+
+if [[ -d backplane-cli ]] ;
+then
+  rm -r ./backplane-cli
+fi
+
+git clone --single-branch --branch master https://github.com/openshift/backplane-cli.git
+pushd backplane-cli
+make build
+mv ocm-backplane /usr/local/bin/ocm-backplane
+chmod 755 /usr/local/bin/ocm-backplane
+
+ocm-backplane completion bash > /etc/bash_completion.d/backplane-cli
+
+popd
+popd
