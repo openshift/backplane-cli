2180d9d7a59bc362ab2270a16177e8885e1c1e0a
Samantha Jayasinghe <sjayasin@redhat.com>
2023-02-22 10:44:23 +1300

    Revert "OSD-14687: Add Backplane CLI managed job add,get,delete,log commands"
    This reverts commit 9366713480a7165587434c79640523d3c9370e10.

--- cmd/ocm-backplane/root.go
+++ cmd/ocm-backplane/root.go
@@ -21,9 +21,8 @@ import (
 	"strings"
 
 	"github.com/openshift/backplane-cli/cmd/ocm-backplane/cloud"
-	"github.com/openshift/backplane-cli/cmd/ocm-backplane/managedJob"
-	"github.com/openshift/backplane-cli/cmd/ocm-backplane/script"
 	"github.com/openshift/backplane-cli/cmd/ocm-backplane/upgrade"
+	"github.com/openshift/backplane-cli/cmd/ocm-backplane/script"
 
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
@@ -85,5 +84,4 @@ func init() {
 	rootCmd.AddCommand(upgrade.UpgradeCmd)
 	rootCmd.AddCommand(cloud.CloudCmd)
 	rootCmd.AddCommand(script.NewScriptCmd())
-	rootCmd.AddCommand(managedJob.NewManagedJobCmd())
 }
