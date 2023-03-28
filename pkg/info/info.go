f60cea5608fb6dd9e185ed8642dd69d249dbbf57
Supreeth Basabattini <sbasabat@redhat.com>
2023-03-28 16:38:42 +0530

    Change config file name

--- pkg/info/info.go
+++ pkg/info/info.go
@@ -10,7 +10,7 @@ const (
 	BACKPLANE_URL_ENV_NAME             = "BACKPLANE_URL"
 	BACKPLANE_PROXY_ENV_NAME           = "HTTPS_PROXY"
 	BACKPLANE_CONFIG_PATH_ENV_NAME     = "BACKPLANE_CONFIG"
-	BACKPLANE_CONFIG_DEFAULT_FILE_NAME = ".backplane.json"
+	BACKPLANE_CONFIG_DEFAULT_FILE_NAME = "backplane.json"
 
 	// GitHub API get fetch the latest tag
 	UpstreamReleaseAPI = "https://api.github.com/repos/openshift/backplane-cli/releases/latest"
