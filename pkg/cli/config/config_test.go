322aace58e1897a89d5d9ff9bc69a57f33103f21
Supreeth Basabattini <sbasabat@redhat.com>
2023-03-29 11:50:28 +0530

    Change default config file path

--- pkg/cli/config/config_test.go
+++ pkg/cli/config/config_test.go
@@ -9,15 +9,25 @@ import (
 func TestGetBackplaneConfigFile(t *testing.T) {
 	t.Run("it returns the Backplane configuration file path if it exists in the user's env", func(t *testing.T) {
 		t.Setenv(info.BACKPLANE_CONFIG_PATH_ENV_NAME, "~/.backplane.stg.env.json")
-		path := GetBackplaneConfigFile()
+		path, err := GetBackplaneConfigFile()
+		if err != nil {
+			t.Error(err)
+		}
 		if path != "~/.backplane.stg.env.json" {
 			t.Errorf("expected path to be %v, got %v", "~/.backplane.stg.env.json", path)
 		}
 	})
 
 	t.Run("it returns the default configuration file path if it does not exist in the user's env", func(t *testing.T) {
-		path := GetBackplaneConfigFile()
-		expectedPath := getConfiDefaultPath(info.BACKPLANE_CONFIG_DEFAULT_FILE_NAME)
+		path, err := GetBackplaneConfigFile()
+		if err != nil {
+			t.Error(err)
+		}
+
+		expectedPath, err := getDefaultConfigPath()
+		if err != nil {
+			t.Error(err)
+		}
 		if path != expectedPath {
 			t.Errorf("expected path to be %v, got %v", expectedPath, path)
 		}
