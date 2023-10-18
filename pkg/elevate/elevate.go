0fefbf2c56776441d97763ffd683254aaefe9795
Bo Meng <bmeng@redhat.com>
2023-10-18 09:30:33 +0800

    Revert "OSD-18058 : Adding a new interactive shell session when we do debug with elevate command "

--- pkg/elevate/elevate.go
+++ pkg/elevate/elevate.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-	"strings"
 
 	logger "github.com/sirupsen/logrus"
 	"k8s.io/client-go/tools/clientcmd/api"
@@ -62,19 +61,12 @@ func RunElevate(argv []string) error {
 
 	logger.Debug("Adding impersonation RBAC allow permissions to kubeconfig")
 
-	elevateCmd := "oc " + strings.Join(argv[1:], " ")
-
-	shell := "/bin/bash"
-
-	if len(argv) > 3 {
-		shell = argv[4]
-	}
+	elevateCmd := argv[1:]
 
 	logger.Debugln("Executing command with temporary kubeconfig as backplane-cluster-admin")
+	ocCmd := ExecCmd("oc", elevateCmd...)
 
-	ocCmd := ExecCmd(shell, "-c", elevateCmd)
 	ocCmd.Env = append(ocCmd.Env, os.Environ()...)
-	ocCmd.Stdin = os.Stdin
 	ocCmd.Stderr = os.Stderr
 	ocCmd.Stdout = os.Stdout
 
@@ -83,7 +75,6 @@ func RunElevate(argv []string) error {
 	}
 
 	err = ocCmd.Run()
-
 	kubeconfigPath, _ := os.LookupEnv("KUBECONFIG")
 	defer func() {
 		logger.Debugln("Command error; Cleaning up temporary kubeconfig")
