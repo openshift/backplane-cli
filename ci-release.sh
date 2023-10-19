697978db2fb30cb561d97d3d3d511d80efaa6867
xiaoyu74 <sbai@redhat.com>
2023-10-19 22:48:13 +1300

    Add CI release script to test release automation

---
+++ ci-release.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+# Extract version from VERSION.md
+VERSION=$(grep 'Version:' VERSION.md | awk '{print $2}')
+
+# Check if version is extracted correctly
+if [ -z "$VERSION" ]; then
+    echo "Error: Failed to extract version from VERSION.md"
+    exit 1
+fi
+
+# Git configurations
+git config user.name "CI release"
+git config user.email "ci-test@release.com"
+
+# Ensure working on the latest main
+git fetch origin
+git checkout origin/main
+
+# Tagging the release
+git tag -a "v${VERSION}" -m "Release v${VERSION}"
+git push origin "v${VERSION}"
