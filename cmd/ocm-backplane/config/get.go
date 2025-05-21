d1b6f61c52393a446bc6f10c2e8dccc491b093a1
Brian Smith <bsmit@redhat.com>
2025-05-21 13:27:27 -0400

    adding get and set for govcloud config

--- cmd/ocm-backplane/config/get.go
+++ cmd/ocm-backplane/config/get.go
@@ -40,13 +40,16 @@ func getConfig(cmd *cobra.Command, args []string) error {
 		fmt.Printf("%s: %s\n", SessionConfigVar, config.SessionDirectory)
 	case PagerDutyAPIConfigVar:
 		fmt.Printf("%s: %s\n", PagerDutyAPIConfigVar, config.PagerDutyAPIKey)
+	case GovcloudVar:
+		fmt.Printf("%s: %t\n", GovcloudVar, config.Govcloud)
 	case "all":
 		fmt.Printf("%s: %s\n", URLConfigVar, config.URL)
 		fmt.Printf("%s: %s\n", ProxyURLConfigVar, proxyURL)
 		fmt.Printf("%s: %s\n", SessionConfigVar, config.SessionDirectory)
 		fmt.Printf("%s: %s\n", PagerDutyAPIConfigVar, config.PagerDutyAPIKey)
+		fmt.Printf("%s: %t\n", GovcloudVar, config.Govcloud)
 	default:
-		return fmt.Errorf("supported config variables are %s, %s, %s & %s", URLConfigVar, ProxyURLConfigVar, SessionConfigVar, PagerDutyAPIConfigVar)
+		return fmt.Errorf("supported config variables are %s, %s, %s, %s, & %s", URLConfigVar, ProxyURLConfigVar, SessionConfigVar, PagerDutyAPIConfigVar, GovcloudVar)
 	}
 
 	return nil
