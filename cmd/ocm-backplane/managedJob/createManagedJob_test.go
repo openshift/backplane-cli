bcdd98289107a62e7094ab0352d2c312314312f0
Dakota Long <dalong@dalong-thinkpadp16vgen1.rmtusnc.csb>
2025-11-17 15:25:25 -0500

    address the case where the JSON200 field does not exist

--- cmd/ocm-backplane/managedJob/createManagedJob_test.go
+++ cmd/ocm-backplane/managedJob/createManagedJob_test.go
@@ -237,6 +237,35 @@ var _ = Describe("managedJob create command", func() {
 			Expect(err.Error()).To(ContainSubstring("error from backplane"))
 		})
 
+		It("should return error instead of panicking when GetRun returns 200 but with invalid response structure", func() {
+			invalidResp := &http.Response{
+				Body:       MakeIoReader(`{}`),
+				Header:     map[string][]string{},
+				StatusCode: http.StatusOK,
+			}
+			invalidResp.Header.Add("Content-Type", "application/json")
+
+			mockOcmInterface.EXPECT().GetOCMEnvironment().Return(ocmEnv, nil).AnyTimes()
+			mockOcmInterface.EXPECT().GetTargetCluster(testClusterID).Return(trueClusterID, testClusterID, nil)
+			mockOcmInterface.EXPECT().IsClusterHibernating(gomock.Eq(trueClusterID)).Return(false, nil).AnyTimes()
+			mockOcmInterface.EXPECT().GetOCMAccessToken().Return(&testToken, nil).AnyTimes()
+			mockClientUtil.EXPECT().MakeRawBackplaneAPIClient("https://newbackplane.url").Return(mockClient, nil)
+			mockClient.EXPECT().GetScriptsByCluster(gomock.Any(), trueClusterID, gomock.Any()).Return(fakeScriptResp, nil)
+			mockClient.EXPECT().CreateJob(gomock.Any(), trueClusterID, gomock.Any()).Return(fakeResp, nil)
+			mockClient.EXPECT().GetRun(gomock.Any(), trueClusterID, gomock.Eq("jid")).Return(invalidResp, nil)
+
+			sut.SetArgs([]string{"create", "SREP/something", "--cluster-id", testClusterID, "--url", "https://newbackplane.url", "--wait"})
+
+			err := sut.Execute()
+
+			Expect(err).ToNot(BeNil())
+			Expect(err.Error()).To(Or(
+				ContainSubstring("received empty response"),
+				ContainSubstring("unable to parse response"),
+				ContainSubstring("job status not found"),
+			))
+		})
+
 		It("should timeout if job waiting in pending status for long time", func() {
 			fakeJobResp = &http.Response{
 				Body:       MakeIoReader(fmt.Sprintf(jobResponseBody, "Pending")),
