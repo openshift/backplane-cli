08ed8a4a9ff6df5c08d28cb88ae6859ee69298b6
Siu Wa Wu <siwu@redhat.com>
2026-09-17 18:00:05 +1000

    remove unused flags

--- cmd/ocm-backplane/testJob/testJob.go
+++ cmd/ocm-backplane/testJob/testJob.go
@@ -13,23 +13,6 @@ func NewTestJobCommand() *cobra.Command {
 		Hidden:       true,
 	}
 
-	// url flag
-	// Denotes backplane url
-	// If this flag is empty, backplane-url will be fetched by user settings.
-	cmd.PersistentFlags().String(
-		"url",
-		"",
-		"Specify backplane url.",
-	)
-
-	// cluster-id Flag
-	cmd.PersistentFlags().StringP("cluster-id", "c", "", "Cluster ID could be cluster name, id or external-id")
-
-	// raw Flag
-	cmd.PersistentFlags().Bool("raw", false, "Prints the raw response returned by the backplane API")
-
-	cmd.PersistentFlags().BoolP("follow", "f", false, "Specify if logs should be streamed")
-
 	cmd.AddCommand(
 		newRenderTestJobCommand(),
 	)
